5bd24d71227a334224f6505cd57db008f0c0fa18
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2024-07-17 16:53:37 +0200

    defines neu

---
+++ src/defines.h
@@ -0,0 +1,16 @@
+//
+//  defines.h
+//  H0_Decoder
+//
+//  Created by Ruedi Heimlicher on 11.09.2020.
+//
+
+#ifndef defines_h
+#define defines_h
+#include <inttypes.h>
+#include <avr/io.h>
+
+#define JOYSTICK_ON 2
+#define CALIB_ON 3
+
+#endif /* defines_h */
